91247175299eb563447f8844339380343892422f
akshaycp05 <akshaycpaiml06@gmail.com>
2026-07-01 12:34:06 +0530

    backend

--- frontend/company.js
+++
@@ -1,2 +0,0 @@
-export const name = "Akshay";
-console.log(name);
--- frontend/talentspark/src/components/footer.tsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-      <footer>
-          <p>© 2024 TalentSpark. All rights reserved.</p>
-      </footer>
-  )
-}
-
-export default Footer;
